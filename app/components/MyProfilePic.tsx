c293ea95e51f7786df27f6f8e13c3210df6863b2
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-08-08 17:10:42 +0200

    really really really ready to vercel

--- app/components/MyProfilePic.tsx
+++ app/components/MyProfilePic.tsx
@@ -6,7 +6,7 @@ export default function MyProfilePic() {
   return (
     <section>
         <Image
-         src="/images/20230514_171044 (1) (1).jpg"
+         src="/images/IMG_20210522_184758_resized_20230324_110358654.jpg"
          width={200}
          height={200}
          alt="nkp"
