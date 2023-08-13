647b08ea8b012f7d273a749409b2a7925bdb53b7
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-08-14 00:58:19 +0200

    res rev

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import MyProfilePic from './components/MyProfilePic'
 import { FaLaptop } from "react-icons/fa"
 import Link from 'next/link'
 
-export const revalidate = 10
+export const revalidate = 86400
 
 
 
