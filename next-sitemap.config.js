eb73a38550df042599f9c09bf762ac9062a3c1e6
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-09-08 20:57:20 +0200

    add url of hostet app

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'http://localhost:3000',
+    siteUrl: process.env.SITE_URL || 'https://myblog-nkped.vercel.app',
     generateRobotsTxt: true,
     generateIndexSitemap: false,
 }
